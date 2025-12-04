2db9ba6409f62d24ba0225b2670cc337e3e9030f
Ahmed Khaled <ahmedali245699@gmail.com>
2025-12-04 14:52:33 +1400

    Implement Security Best Practices: Rate Limiting, Helmet, Data Sanitization (NoSQL/XSS)

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,26 @@ const httpStatusText = require('./utils/httpStatusText');
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    message: 'Too many accounts created from this IP, please try again after 15 minutes'
+});
+app.use('/api',limiter);
+app.use(express.json({limit:'10kb'}));
+app.use(mongoSanitize());
+app.use(xss());
 app.use('/api/products',productRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/orders',orderRoutes)
